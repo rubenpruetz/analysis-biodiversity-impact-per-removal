6886933acaacbc7cc24ca0bcecd191ee960b32ae
Ruben Prütz <rubenpruetz@gmail.com>
2025-07-08 15:40:33 +0200

    Added supplementary code to show BECCS in refugia for different UNFCCC annex groups

--- scripts/4_supplementary_data_analysis.py
+++ scripts/4_supplementary_data_analysis.py
@@ -4,12 +4,94 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
+from required_functions import *
 plt.rcParams.update({'figure.dpi': 600})
 
 path_ar6_data = Path('/Users/rpruetz/Documents/phd/datasets')
 path_all = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity')
+
+path_globiom = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/globiom_maps')
+path_aim = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/aim_maps')
+path_image = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/image_maps')
+
 ar6_db = pd.read_csv(path_ar6_data / 'AR6_Scenarios_Database_World_v1.1.csv')
 energy_crop_share = pd.read_csv(path_all / 'share_energy_crops_estimates.csv')
+
+# %% plot supplementary figure on BECCS in Annex-I and Non-Annex I refugia
+
+paths = {'GLOBIOM': path_globiom, 'AIM': path_aim, 'IMAGE': path_image}
+area_df = load_and_concat('area_df_annex_group_temp_decline_not_allowed', paths)
+
+rcps = ['19', '26', '45']  # specify RCPs that shall be plotted
+area_df = area_df.loc[area_df['RCP'].isin(rcps)]
+
+rcp_pal = {'19': '#00adcf', '26': '#173c66', '34': '#f79320',
+           '45': '#e71d24', '60': '#951b1d', 'Baseline': 'dimgrey'}
+all_rcps = sorted(area_df['RCP'].unique())
+
+fig, axes = plt.subplots(3, 2, figsize=(8, 8), sharex=True, sharey=False)
+sns.lineplot(data=area_df.query('Model == "AIM" & mitigation_option == "BECCS"'),
+             x='Year', y='alloc_perc_ag1', palette=rcp_pal, hue='RCP',
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[0, 0])
+sns.lineplot(data=area_df.query('Model == "GLOBIOM" & mitigation_option == "BECCS"'),
+             x='Year', y='alloc_perc_ag1', palette=rcp_pal, hue='RCP',
+             errorbar=('pi', 100), estimator='median', legend=True, ax=axes[1, 0])
+sns.lineplot(data=area_df.query('Model == "IMAGE" & mitigation_option == "BECCS"'),
+             x='Year', y='alloc_perc_ag1', palette=rcp_pal, hue='RCP',
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[2, 0])
+
+sns.lineplot(data=area_df.query('Model == "AIM" & mitigation_option == "BECCS"'),
+             x='Year', y='alloc_perc_ag2', palette=rcp_pal, hue='RCP',
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[0, 1])
+sns.lineplot(data=area_df.query('Model == "GLOBIOM" & mitigation_option == "BECCS"'),
+             x='Year', y='alloc_perc_ag2', palette=rcp_pal, hue='RCP',
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[1, 1])
+sns.lineplot(data=area_df.query('Model == "IMAGE" & mitigation_option == "BECCS"'),
+             x='Year', y='alloc_perc_ag2', palette=rcp_pal, hue='RCP',
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[2, 1])
+
+handles, labels = axes[1, 0].get_legend_handles_labels()
+rename_dict = {'19': '1.5 °C', '26': '2 °C', '45': 'Current Policies'}
+
+new_labels = [rename_dict.get(label, label) for label in labels]
+
+axes[1, 0].legend(handles, new_labels, bbox_to_anchor=(-0.02, 2.7), loc='upper left',
+                  ncols=3, columnspacing=1, handletextpad=0.4)
+
+axes[0, 0].set_title('Annex I\n(BECCS)')
+axes[0, 1].set_title('Non-Annex I\n(BECCS)')
+
+axes[2, 0].set_xlabel('')
+axes[2, 1].set_xlabel('')
+
+axes[0, 0].set_ylabel('AIM')
+axes[1, 0].set_ylabel('GLOBIOM')
+axes[2, 0].set_ylabel('IMAGE')
+
+axes[0, 1].set_ylabel('')
+axes[1, 1].set_ylabel('')
+axes[2, 1].set_ylabel('')
+
+for ax in axes[:, 0]:
+    ax.set_yticks([0, 1, 2, 3, 4, 5])
+    ax.set_ylim([0, 5.5])
+for ax in axes[:, 1]:
+    ax.set_yticks([0, 1, 2, 3, 4, 5])
+    ax.set_ylim([0, 5.5])
+    ax.set_yticklabels([])
+
+fig.supylabel(f'Share of remaining refugia allocated for CDR [%] (SSP1-SSP3 range as shading)',
+              x=0.05, va='center', ha='center')
+
+for ax in axes.flat:
+    ax.set_xlim(2020, 2100)
+    ax.set_xticks([2020, 2100])
+    ax.grid(True, axis='y', linestyle='--', linewidth=0.5, alpha=0.8)
+
+plt.subplots_adjust(hspace=0.25)
+plt.subplots_adjust(wspace=0.2)
+sns.despine()
+plt.show()
 
 # %% get AR6 land cover data for SSP-RCP combinations
 years = ['2010', '2020', '2030', '2040', '2050', '2060', '2070', '2080',
@@ -160,3 +242,5 @@ plt.subplots_adjust(hspace=0.1)
 plt.subplots_adjust(wspace=0.25)
 sns.despine()
 plt.show()
+
+# %%
